9a4420ea8d933fa395ee254c3eb82ea99e5c9f4b
KeltonKarboviak <kelton.karboviak@gmail.com>
2018-03-12 21:43:06 -0500

    Added fabile for deploying to server

---
+++ fabfile.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+
+from fabric.api import *
+
+env.use_ssh_config = True
+env.hosts = ['deploy-fdm-server']
+env.directory = '/home/fdm/sync'
+env.venv_directory = 'venv'
+env.activate = 'source ' + os.path.join(env.directory, env.venv_directory, 'bin/activate')
+env.repo = 'https://github.com/KeltonKarboviak/ngafid_sync.git'
+
+@task(default=True)
+def deploy():
+    pull_changes()
+    run_pip()
+    update_permissions()
+
+
+def pull_changes():
+    with cd(env.directory):
+        run('git pull')
+
+
+def run_pip():
+    with cd(env.directory), prefix(env.activate):
+        run('pip install -r requirements.txt')
+
+
+def update_permissions():
+    with cd(env.directory):
+        run('chmod 770 .env')
